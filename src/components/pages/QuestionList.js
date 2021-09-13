aab5575928271f4b030ec3b64a2e879bf44e00d6
nhozsoy <nhozsoy@hotmail.com>
2021-09-13 21:40:01 +0300

    Fetch questions from api

--- src/components/pages/QuestionList.js
+++ src/components/pages/QuestionList.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../../App.css';
 
 export default function QuestionList() {
+  const [questions, setQuestions] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://polls.apiblueprint.org/questions")
+      .then(response => {
+        if (response.data) setQuestions(response.data);
+      })
+      .catch(error => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  }, []);
+
+  const renderErrorMessage = () => {
+    return (
+      <p className='error-message'>{error}</p>
+    )
+  };
+
+  const renderLoadingMessage = () => {
+    return (
+      <p>Loading...</p>
+    )
+  };
+
+  const renderQuestions = () => {
+    return (
+      <p>Questions</p>
+    )
+  };
+
   return (
-    <div>
+    <div className='questions-list-page'>
       <h1>Question List</h1>
+      {error && renderErrorMessage()}
+      {loading && renderLoadingMessage()}
+      {questions && renderQuestions()}
     </div>
   );
 }
